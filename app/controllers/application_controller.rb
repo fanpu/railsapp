380394e44e4c421a57add78474b335065f7b236f
Fan Pu <zengfanpu@gmail.com>
2015-03-24 21:02:37 +0800

    slight edit to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello, world!"
+    render text: "chemdank - powered by ruby on rails. coming your way soon!"
   end
 end
